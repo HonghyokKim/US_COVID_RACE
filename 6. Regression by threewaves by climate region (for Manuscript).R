16ed612bf13ad0e47b28b90d4f7477321c139948
Honghyok Kim <68622296+HonghyokKim@users.noreply.github.com>
2021-07-02 12:29:08 -0400

    Update 6. Regression by threewaves by climate region (for Manuscript).R

--- 6. Regression by threewaves by climate region (for Manuscript).R
+++ 6. Regression by threewaves by climate region (for Manuscript).R
@@ -3,9 +3,9 @@ library(glmmTMB)
 
 
 data.wave<-vector("list",3)
-data.wave[[1]]<-read.csv("C:\\Users\\hongh\\Dropbox (Yale_FES)\\COVID19\\USCOVID\\Race_COVID\\finaldat\\fin\\Wave1.csv")
-data.wave[[2]]<-read.csv("C:\\Users\\hongh\\Dropbox (Yale_FES)\\COVID19\\USCOVID\\Race_COVID\\finaldat\\fin\\Wave2.csv")
-data.wave[[3]]<-read.csv("C:\\Users\\hongh\\Dropbox (Yale_FES)\\COVID19\\USCOVID\\Race_COVID\\finaldat\\fin\\Wave3.csv")
+data.wave[[1]]<-read.csv("PATHNAME\Wave1.csv")
+data.wave[[2]]<-read.csv("PATHNAME\\Wave2.csv")
+data.wave[[3]]<-read.csv("PATHNAME\\Wave3.csv")
 
 for (ii in seq(3)) {
   data.wave[[ii]]$climate2 <- data.wave[[ii]]$climate
@@ -467,7 +467,7 @@ for (aa in seq(n_cl)) {
 
 
 
-jpeg("C:/Users/hongh/Dropbox (Yale_FES)/COVID19/USCOVID/Race_COVID/Manuscript/Figures/Figure S2.jpg",
+jpeg("PATHNAME/Figure SS.jpg",
      width=16,height=24,units="in",res=900,pointsize=25)
 par(mfrow=c(5,1),mar=c(4,4,2,1),oma=c(0,0,1,0))
 
@@ -550,9 +550,6 @@ dev.off()
 
 
 
-
-
-save.image("C:\\Users\\hongh\\Dropbox (Yale_FES)\\COVID19\\USCOVID\\Race_COVID\\Manuscript\\Rimages\\Cases_by_ClimateRegion.Rdata")
 
 
 
